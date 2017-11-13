785077fb56800d5049cd6bda9fa64b8559f59458
Hiyori <Hiyori@SimbaMoe.cn>
2017-11-13 09:05:43 +0800

    修改流量包扣费设置

--- app/Console/Commands/AutoDecGoodsTrafficJob.php
+++ app/Console/Commands/AutoDecGoodsTrafficJob.php
@@ -35,11 +35,11 @@ class AutoDecGoodsTrafficJob extends Command
 
                 // 流量包到期自动扣总流量
                 //if ($goods->type == 1) {
-                    if ($u->transfer_enable - $goods->traffic * 1048576 <= 0) {
-                        User::query()->where('id', $og->user_id)->update(['transfer_enable' => 0]);
-                    } else {
-                        User::query()->where('id', $og->user_id)->decrement('transfer_enable', $goods->traffic * 1048576);
-                    }
+                    // if ($u->transfer_enable - $goods->traffic * 1048576 <= 0) {
+                    //     User::query()->where('id', $og->user_id)->update(['transfer_enable' => 0]);
+                    // } else {
+                    //     User::query()->where('id', $og->user_id)->decrement('transfer_enable', $goods->traffic * 1048576);
+                    // }
                 //}
 
                 OrderGoods::query()->where('id', $og->id)->update(['is_expire' => 1]);
