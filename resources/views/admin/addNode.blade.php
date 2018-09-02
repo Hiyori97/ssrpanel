78fa2a6c78987aba3df78531151b5672ec67a1eb
Hiyori <Hiyori@SimbaMoe.cn>
2018-09-02 20:40:23 +0800

    单端口多用户添加chain_d支持

--- resources/views/admin/addNode.blade.php
+++ resources/views/admin/addNode.blade.php
@@ -178,6 +178,7 @@
                                                                 <option value="auth_aes128_md5">auth_aes128_md5</option>
                                                                 <option value="auth_aes128_sha1">auth_aes128_sha1</option>
                                                                 <option value="auth_chain_a" selected>auth_chain_a</option>
+                                                                <option value="auth_chain_d" selected>auth_chain_d</option>
                                                             </select>
                                                             <span class="help-block"> 展示和生成配置用，后端配置注意保持一致 </span>
                                                         </div>
